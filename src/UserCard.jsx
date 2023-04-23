30db30afa1186920f7e0cede065935f007fce9e4
Laura Cristina <lauracristina.s@hotmail.com>
2023-04-23 18:18:33 -0300

    atualizando css

--- src/UserCard.jsx
+++ src/UserCard.jsx
@@ -3,10 +3,12 @@ import React from "react";
 const UserCard = ({ name, title, description, photo }) => {
   return (
     <div className="user-card">
-      <img src={photo} alt={name} />
-      <h2>{name}</h2>
-      <h3>{title}</h3>
-      <p>{description}</p>
+      <img src={photo} alt={name}/>
+      <div className="user-card-content">
+        <h2>{name}</h2>
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
     </div>
   );
 };
